3c08518f58a12e181cb20bb5306a6fdcd9be1ea1
Jonathan Hamstra <jhamstra24@my.whitworth.edu>
2024-04-29 11:07:59 -0700

    Added python script to pull info of person based on PersonID

---
+++ website/python/IDSearch.py
@@ -0,0 +1,13 @@
+import sys
+file = open("../../backup/individuals.csv", "r")
+file.readline()
+
+ID = input("What Person ID are you looking for?")
+
+for line in file:
+    split = line.split(",")
+    if ID == split[1]:
+        print("Here is the data on Person ID %s \n %s" % (ID, line))
+        sys.exit()
+print("Sorry, we were unable to find anyone with that ID.")
+file.close()
