9921eb829cba7f99fc9431da6408dabeb3ebac4d
Jay Phillips <jayphillips23@my.whitworth.edu>
2024-05-06 19:55:42 -0700

    Button Working

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", context="Testing") # This sitring is displayed on the home page
 
+@app.route("/userScore", methods=["POST"])
+def userScore():
+    return render_template("userScore.html", context="Testing")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
